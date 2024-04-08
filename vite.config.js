e51775fcda224ba774ca3c03240bbe493ee0d4c5
Shirota <artem.chmil@bk.ru>
2024-04-08 16:57:39 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/Components"),
+      "@hooks": path.resolve(__dirname, "src/Hooks"),
+      "@Ui": path.resolve(__dirname, "src/UI"),
+      "@Utils": path.resolve(__dirname, "src/Utils"),
+
+    },
+  },
+})
