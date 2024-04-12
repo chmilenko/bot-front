49d586ad41d4e75565fef8499c1e4649ced6ce35
Shirota <artem.chmil@bk.ru>
2024-04-12 17:46:52 +0300

    fix sizes

--- src/Features/ModelInfo/ModelInfo.jsx
+++ src/Features/ModelInfo/ModelInfo.jsx
@@ -15,7 +15,6 @@ function ModelInfo() {
     36, 36.5, 37, 37.5, 38, 38.5, 39, 39.5, 40, 40.5, 41, 41.5, 42, 42.5, 43,
     43.5, 44, 44.5, 45, 45.5, 46, 46.5, 47,
   ];
-  console.log(oneModel.data.sizes);
   let navigate = useNavigate();
   console.log(oneModel);
   return (
@@ -35,7 +34,7 @@ function ModelInfo() {
             <button
               key={i}
               className="size"
-              disabled={!oneModel?.data?.sizes?.some((s) => s.Size === size)}
+              disabled={!oneModel?.data?.sizes?.some((s) => s.size === size)}
             >
               {size}
             </button>
